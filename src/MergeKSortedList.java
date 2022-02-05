dfcf86135930bff72192fa30e0f3b909c41327c5
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-02-05 10:30:06 +0530

    Merge K Sorted List optimized using Divide and conquer

--- src/MergeKSortedList.java
+++ src/MergeKSortedList.java
@@ -1,10 +1,27 @@
 public class MergeKSortedList {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode ans = null;
-        for(int i=0;i<lists.length;i++) {
-            ans = mergeSortedList(ans, lists[i]);
-        }
-        return ans;
+        /**
+         * FIRST APPROACH (NOT OPTIMIZED)
+         * ListNode ans = null;
+         * for(int i=0;i<lists.length;i++) {
+         * ans = mergeSortedList(ans, lists[i]);
+         * }
+         * return ans;
+         * */
+
+        /**
+         * SECOND APPROACH (OPTIMIZED)
+         * DIVIDE AND CONQUER
+         * */
+        if(lists.length == 0) return null;
+        return mergeKListsUtil(lists,0, lists.length-1);
+    }
+    private ListNode mergeKListsUtil(ListNode[] lists, int start, int end){
+        if(start == end) return lists[start];
+        int mid = (start + end)/2;
+        ListNode left = mergeKListsUtil(lists,start,mid);
+        ListNode right = mergeKListsUtil(lists,mid+1,end);
+        return mergeSortedList(left,right);
     }
     private ListNode mergeSortedList(ListNode head1, ListNode head2){
         ListNode ansHead = new ListNode();
