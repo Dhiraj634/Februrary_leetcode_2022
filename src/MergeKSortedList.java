b666260f184af52a70d3511bd82280ed81e143c9
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-02-05 09:52:35 +0530

    MergeKSortedList

---
+++ src/MergeKSortedList.java
@@ -0,0 +1,35 @@
+public class MergeKSortedList {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for(int i=0;i<lists.length;i++) {
+            ans = mergeSortedList(ans, lists[i]);
+        }
+        return ans;
+    }
+    private ListNode mergeSortedList(ListNode head1, ListNode head2){
+        ListNode ansHead = new ListNode();
+        ListNode temp = ansHead;
+        while(head1!=null && head2!=null){
+            if(head1.val <= head2.val){
+                temp.next = head1;
+                ListNode node =  head1.next;
+                head1.next = null;
+                head1 = node;
+            }else{
+                temp.next = head2;
+                ListNode node =  head2.next;
+                head2.next = null;
+                head2 = node;
+            }
+            temp = temp.next;
+        }
+        if(head1 != null){
+            temp.next = head1;
+        }
+        if(head2 != null){
+            temp.next = head2;
+        }
+        return ansHead.next;
+
+    }
+}
