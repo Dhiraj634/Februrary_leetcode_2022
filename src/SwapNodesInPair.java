2c79d111dca608f21546d620ca9a6eabcceffde3
Dhiraj Kumar Yadav <dhiru634@gmail.com>
2022-02-16 09:54:51 +0530

    Swap Nodes in Pair - improved code readability

--- src/SwapNodesInPair.java
+++ src/SwapNodesInPair.java
@@ -2,25 +2,18 @@ public class SwapNodesInPair {
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
         ListNode ans = new ListNode();
-        ListNode tempHead = ans,odd = head,even = head.next;
-        while(odd !=null && even!=null){
-            ListNode nextOdd = even.next;
-
-            tempHead.next = even;
-            tempHead.next.next = odd;
-
-            if(nextOdd != null) {
-                even = nextOdd.next;
-            }else{
-                even = null;
-            }
-
-            odd = nextOdd;
+        ListNode tempHead = ans;
+        while(head !=null && head.next!=null){
+            ListNode next = head.next.next;
+            tempHead.next = head.next;
+            tempHead.next.next = head;
+            head = next;
             tempHead = tempHead.next.next;
         }
-        if(odd != null){
-            tempHead.next = odd;
-            tempHead=tempHead.next;
+        if(head!=null)
+        {
+            tempHead.next = head;
+            tempHead = tempHead.next;
         }
         tempHead.next = null;
         return ans.next;
